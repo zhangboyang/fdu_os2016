52e708df0fa0c003f2c548958543d8775dd29777
autocommit <autocommit@nowhere.com>
2016-09-20 20:43:47 +0800

    autocommit

--- include/arch/i386/arch-boot.h
+++ include/arch/i386/arch-boot.h
@@ -19,6 +19,47 @@
 #ifndef _ARCH_BOOT_H
 #define _ARCH_BOOT_H
 
+
+/*
+    ZBY: some macros copied from xv6
+*/
+
+
+//
+// assembler macros to create x86 segments
+//
+
+#define SEG_NULLASM                                             \
+        .word 0, 0;                                             \
+        .byte 0, 0, 0, 0
+
+// The 0xC0 means the limit is in 4096-byte units
+// and (for executable segments) 32-bit mode.
+#define SEG_ASM(type,base,lim)                                  \
+        .word (((lim) >> 12) & 0xffff), ((base) & 0xffff);      \
+        .byte (((base) >> 16) & 0xff), (0x90 | (type)),         \
+                (0xC0 | (((lim) >> 28) & 0xf)), (((base) >> 24) & 0xff)
+
+#define STA_X     0x8       // Executable segment
+#define STA_E     0x4       // Expand down (non-executable segments)
+#define STA_C     0x4       // Conforming code segment (executable only)
+#define STA_W     0x2       // Writeable (non-executable segments)
+#define STA_R     0x2       // Readable (executable segments)
+#define STA_A     0x1       // Accessed
+
+
+
+// various segment selectors.
+#define SEG_KCODE 1  // kernel code
+#define SEG_KDATA 2  // kernel data+stack
+#define SEG_KCPU  3  // kernel per-cpu data
+#define SEG_UCODE 4  // user code
+#define SEG_UDATA 5  // user data+stack
+#define SEG_TSS   6  // this process's task state
+
+
+
+                
 #ifndef __ASSEMBLER__
 
 #endif /* !__ASSEMBLER__ */
