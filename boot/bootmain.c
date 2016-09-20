371ff41c0903dce79295276ba57628d0a70271cb
autocommit <autocommit@nowhere.com>
2016-09-21 00:07:08 +0800

    autocommit

--- boot/bootmain.c
+++ boot/bootmain.c
@@ -58,7 +58,7 @@ void bootmain(void)
     }
 
     // call entry point, should not return
-    (( void (__noreturn *)(void)) (elf->e_entry)) ();
+    ((void (__attribute__((noreturn)) *)(void)) (elf->e_entry)) ();
     
 //    boot_panic();
 }
