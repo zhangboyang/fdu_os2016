1eee83fa158938f0a9fb78f2f4a2218b994a8e47
autocommit <autocommit@nowhere.com>
2016-09-21 00:14:04 +0800

    autocommit

--- boot/bootmain.c
+++ boot/bootmain.c
@@ -61,6 +61,6 @@ void bootmain(void)
     ((void (*)(void)) (elf->e_entry)) ();
     
     boot_panic();
-    void (__attribute__((noreturn)) ****f) (void);
+    void (*f) (void) __attribute__((noreturn));
 }
 
