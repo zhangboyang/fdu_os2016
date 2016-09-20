735a1d8472d3269ca8a44e36c7e86f9488d167b8
autocommit <autocommit@nowhere.com>
2016-09-20 23:35:59 +0800

    autocommit

--- boot/arch/i386/kernreader.c
+++ boot/arch/i386/kernreader.c
@@ -9,7 +9,7 @@
 static inline unsigned char inb(unsigned short port)
 {
     unsigned char data;
-    __asm__ __volatile__ ("in %1,%0" : "=a" (data) : "d" (port));
+    __asm__ __volatile__ ("inb %1, %0" : "=a"(data) : "d"(port));
     return data;
 }
 
