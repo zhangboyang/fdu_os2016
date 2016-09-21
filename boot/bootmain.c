19933bbc30e796cd9a8754fe5e8aa3c2e7966e5d
autocommit <autocommit@nowhere.com>
2016-09-21 20:07:47 +0800

    autocommit

--- boot/bootmain.c
+++ boot/bootmain.c
@@ -40,7 +40,7 @@ void bootmain(void)
         " /_/    \\_\\_____|_|  |_| \\_/  \\___/ \n"
     );
 
-    bprintf("hello %s %x %u %d %c %s", "a", 0x80000000,  0x80000000,  0x80000000, 'b', "aaaaadskfjdaksfjkdsfh2zzzzz1234");
+    bprintf("hello %s %x %u %d %c %s", "a", 0x80000000,  0xffffffff,  0x80000000, 'b', "aaaaadskfjdaksfjkdsfh2zzzzz1234");
     
     while (1);
     static unsigned char elfbuf[BOOT_ELF_PRELOAD];
