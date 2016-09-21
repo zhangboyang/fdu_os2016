4cf8f2f39bc4e620f77aa47221394f128d78bdc6
autocommit <autocommit@nowhere.com>
2016-09-21 22:05:50 +0800

    autocommit

--- boot/arch/i386/stage0.c
+++ boot/arch/i386/stage0.c
@@ -36,7 +36,7 @@
 
 static void __entry stage0_memset(void *s, int c, size_t n)
 {
-    __asm__ __volatile__ ("rep movs"::"a"(c), "D"(s), "c"(n): "memory", "cc", "eax", "edi", "ecx");
+    __asm__ __volatile__ ("rep movs"::"a"(c), "D"(s), "c"(n): "memory", "cc");
 }
 
 static unsigned char __entry stage0_inb(unsigned short port)
