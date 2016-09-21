82d0b0c52c69545b03dbf2fee94ed532ce7bd836
autocommit <autocommit@nowhere.com>
2016-09-21 20:22:30 +0800

    autocommit

--- boot/bootmain.c
+++ boot/bootmain.c
@@ -56,7 +56,6 @@ void bootmain(void)
         elf->e_ident[EI_MAG2] != ELFMAG2 ||
         elf->e_ident[EI_MAG3] != ELFMAG3) boot_panic("ELF magic mismatch!");
 
-boot_panic("ahaaa");
     // load each program segment (ignores ph flags)
     struct elf_phdr *ph, *eph;
     ph = (void *) elf + elf->e_phoff;
