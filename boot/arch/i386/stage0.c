2bfc99c07fea75045d32cb96336ab5433f8177c1
autocommit <autocommit@nowhere.com>
2016-09-21 11:14:50 +0800

    autocommit

--- boot/arch/i386/stage0.c
+++ boot/arch/i386/stage0.c
@@ -9,11 +9,31 @@
  */
 
 
-
 #define stage0_panic() while (1)
 #define BOOT_ELF_PRELOAD 4096
 
 #define __entry __attribute__((section(".entry")))
+
+
+static unsigned char __entry inb(unsigned short port)
+{
+    unsigned char data;
+    __asm__ __volatile__ ("inb %1, %0": "=a"(data): "d"(port));
+    return data;
+}
+static void __entry outb(unsigned short port, unsigned char data)
+{
+    __asm__ __volatile__ ("outb %0, %1":: "a"(data), "d"(port));
+}
+static void __entry insl(int port, void *addr, int cnt)
+{
+    __asm __volatile__ ("cld; rep insl" :
+                        "=D" (addr), "=c" (cnt) :
+                        "d" (port), "0" (addr), "1" (cnt) :
+                        "memory", "cc");
+}
+
+
 
 /*
     bootloader stage0:
