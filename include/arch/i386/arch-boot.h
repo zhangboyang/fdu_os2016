e81751938a7619336a074b716eaabcd4d2d62b08
autocommit <autocommit@nowhere.com>
2016-09-21 19:46:48 +0800

    autocommit

--- include/arch/i386/arch-boot.h
+++ include/arch/i386/arch-boot.h
@@ -102,6 +102,9 @@ static inline void insl(int port, void *addr, int cnt)
 
 // there is no "stdarg.h" in bootloader
 // we should add it by ourselves
+// taken from GCC's stdarg.h
+typedef __builtin_va_list __gnuc_va_list;
+typedef __gnuc_va_list va_list;
 #define va_start(v,l)   __builtin_va_start(v,l)
 #define va_end(v)   __builtin_va_end(v)
 #define va_arg(v,l) __builtin_va_arg(v,l)
