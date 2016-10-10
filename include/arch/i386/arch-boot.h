2570e2b2298fed65153559c42d79972684df0322
autocommit <autocommit@nowhere.com>
2016-10-10 22:13:06 +0800

    autocommit

--- include/arch/i386/arch-boot.h
+++ include/arch/i386/arch-boot.h
@@ -24,53 +24,7 @@
     ZBY: some macros copied from xv6
 */
 
-
-//
-// assembler macros to create x86 segments
-//
-
-#define SEG_NULLASM                                             \
-        .word 0, 0;                                             \
-        .byte 0, 0, 0, 0
-
-// The 0xC0 means the limit is in 4096-byte units
-// and (for executable segments) 32-bit mode.
-#define SEG_ASM(type,base,lim)                                  \
-        .word (((lim) >> 12) & 0xffff), ((base) & 0xffff);      \
-        .byte (((base) >> 16) & 0xff), (0x90 | (type)),         \
-                (0xC0 | (((lim) >> 28) & 0xf)), (((base) >> 24) & 0xff)
-
-#define STA_X     0x8       // Executable segment
-#define STA_E     0x4       // Expand down (non-executable segments)
-#define STA_C     0x4       // Conforming code segment (executable only)
-#define STA_W     0x2       // Writeable (non-executable segments)
-#define STA_R     0x2       // Readable (executable segments)
-#define STA_A     0x1       // Accessed
-
-
-
-// various segment selectors.
-#define SEG_KCODE 1  // kernel code
-#define SEG_KDATA 2  // kernel data+stack
-#define SEG_KCPU  3  // kernel per-cpu data
-#define SEG_UCODE 4  // user code
-#define SEG_UDATA 5  // user data+stack
-#define SEG_TSS   6  // this process's task state
-
-
-// Control Register flags
-#define CR0_PE          0x00000001      // Protection Enable
-#define CR0_MP          0x00000002      // Monitor coProcessor
-#define CR0_EM          0x00000004      // Emulation
-#define CR0_TS          0x00000008      // Task Switched
-#define CR0_ET          0x00000010      // Extension Type
-#define CR0_NE          0x00000020      // Numeric Errror
-#define CR0_WP          0x00010000      // Write Protect
-#define CR0_AM          0x00040000      // Alignment Mask
-#define CR0_NW          0x20000000      // Not Writethrough
-#define CR0_CD          0x40000000      // Cache Disable
-#define CR0_PG          0x80000000      // Paging
-
+#include <mmu.h>
 
                 
 #ifndef __ASSEMBLER__
