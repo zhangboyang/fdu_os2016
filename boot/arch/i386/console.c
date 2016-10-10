3020d43bc359207130e88f1874ee6e884a81fbb5
autocommit <autocommit@nowhere.com>
2016-10-10 20:12:54 +0800

    autocommit

--- boot/arch/i386/console.c
+++ boot/arch/i386/console.c
@@ -13,6 +13,9 @@
 
 // most codes below are copied from JOS (MIT 6.828)
 
+/***** Text-mode CGA/VGA display output *****/
+
+
 #define MONO_BASE	0x3B4
 #define MONO_BUF	0xB0000
 #define CGA_BASE	0x3D4
@@ -112,13 +115,113 @@ cga_putc(int c)
 	outb(addr_6845 + 1, crt_pos);
 }
 
+
+// Stupid I/O delay routine necessitated by historical PC design flaws
+static void
+delay(void)
+{
+	inb(0x84);
+	inb(0x84);
+	inb(0x84);
+	inb(0x84);
+}
+
+/***** Serial I/O code *****/
+
+#define COM1		0x3F8
+
+#define COM_RX		0	// In:	Receive buffer (DLAB=0)
+#define COM_TX		0	// Out: Transmit buffer (DLAB=0)
+#define COM_DLL		0	// Out: Divisor Latch Low (DLAB=1)
+#define COM_DLM		1	// Out: Divisor Latch High (DLAB=1)
+#define COM_IER		1	// Out: Interrupt Enable Register
+#define   COM_IER_RDI	0x01	//   Enable receiver data interrupt
+#define COM_IIR		2	// In:	Interrupt ID Register
+#define COM_FCR		2	// Out: FIFO Control Register
+#define COM_LCR		3	// Out: Line Control Register
+#define	  COM_LCR_DLAB	0x80	//   Divisor latch access bit
+#define	  COM_LCR_WLEN8	0x03	//   Wordlength: 8 bits
+#define COM_MCR		4	// Out: Modem Control Register
+#define	  COM_MCR_RTS	0x02	// RTS complement
+#define	  COM_MCR_DTR	0x01	// DTR complement
+#define	  COM_MCR_OUT2	0x08	// Out2 complement
+#define COM_LSR		5	// In:	Line Status Register
+#define   COM_LSR_DATA	0x01	//   Data available
+#define   COM_LSR_TXRDY	0x20	//   Transmit buffer avail
+#define   COM_LSR_TSRE	0x40	//   Transmitter off
+
+static bool serial_exists;
+
+static int
+serial_proc_data(void)
+{
+	if (!(inb(COM1+COM_LSR) & COM_LSR_DATA))
+		return -1;
+	return inb(COM1+COM_RX);
+}
+
+void
+serial_intr(void)
+{
+	if (serial_exists)
+		cons_intr(serial_proc_data);
+}
+
+static void
+serial_putc(int c)
+{
+	int i;
+
+	for (i = 0;
+	     !(inb(COM1 + COM_LSR) & COM_LSR_TXRDY) && i < 12800;
+	     i++)
+		delay();
+
+	outb(COM1 + COM_TX, c);
+}
+
+static void
+serial_init(void)
+{
+	// Turn off the FIFO
+	outb(COM1+COM_FCR, 0);
+
+	// Set speed; requires DLAB latch
+	outb(COM1+COM_LCR, COM_LCR_DLAB);
+	outb(COM1+COM_DLL, (uint8_t) (115200 / 9600));
+	outb(COM1+COM_DLM, 0);
+
+	// 8 data bits, 1 stop bit, parity off; turn off DLAB latch
+	outb(COM1+COM_LCR, COM_LCR_WLEN8 & ~COM_LCR_DLAB);
+
+	// No modem controls
+	outb(COM1+COM_MCR, 0);
+	// Enable rcv interrupts
+	outb(COM1+COM_IER, COM_IER_RDI);
+
+	// Clear any preexisting overrun indications and interrupts
+	// Serial port doesn't exist if COM_LSR returns 0xFF
+	serial_exists = (inb(COM1+COM_LSR) != 0xFF);
+	(void) inb(COM1+COM_IIR);
+	(void) inb(COM1+COM_RX);
+
+}
+
+
+
+
+
+
+
 void bioinit() // bootloader io system init
 {
     cga_init();
+    serial_init();
 }
 
 void bputc(int c)
 {
     cga_putc(c);
+    serial_putc(c);
 }
 
