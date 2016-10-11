5fb890d2f05cdd8bb3089483773cf3d5c7d6e966
autocommit <autocommit@nowhere.com>
2016-10-11 13:05:59 +0800

    autocommit

---
+++ lib/libc/string.h
@@ -0,0 +1,15 @@
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif /* HAVE_CONFIG_H */
+
+/*
+ * this file is added by ZBY
+ */
+ 
+#ifndef _LIBC_STRING_H
+#define _LIBC_STRING_H
+
+void *memset(void *s, int c, size_t n);
+
+#endif /* _LIBC_STRING_H */
+
