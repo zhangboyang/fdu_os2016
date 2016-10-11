ca2347a26000fe9db78e60ff8355e7972f641896
autocommit <autocommit@nowhere.com>
2016-10-11 13:12:08 +0800

    autocommit

--- include/arch/i386/arch-mmu.h
+++ include/arch/i386/arch-mmu.h
@@ -124,9 +124,9 @@ typedef pte_t pgtable_t;
 
 
 #ifndef PREMAP
-#define KVA2PA(x) (x)
+#define KVA2PA(x) ((addr_t) (x))
 #else
-#define KVA2PA(x) (VMA2LMA(x))
+#define KVA2PA(x) ((addr_t) (VMA2LMA(x))
 #endif
 
 // make a pgindex_t points to pgmid_t
