905eed946925ed41a379af0753876f44df2cac0b
David Gao <davidgao1001@gmail.com>
2016-10-18 03:56:53 +0800

    ADD: additional resources for lab2.1

--- lib/libc/string.h
+++ lib/libc/string.h
@@ -1,4 +1,4 @@
-/* Copyright (C) 2016 David Gao <davidgao1001@gmail.com>
+/* Copyright (C) 2016 Gan Quan <coin2028@hotmail.com>
  *
  * This file is part of AIM.
  *
@@ -16,31 +16,24 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#ifdef HAVE_CONFIG_H
-#include <config.h>
-#endif /* HAVE_CONFIG_H */
+#ifndef _STRING_H
+#define _STRING_H
 
 #include <sys/types.h>
-#include <aim/early_kmmap.h>
-#include <aim/mmu.h>
-#include <libc/string.h>
 
-bool early_mapping_valid(struct early_mapping *entry)
-{
-	return true;
-}
+#ifndef __ASSEMBLER__
 
-void page_index_clear(pgindex_t *index)
-{
-}
+void *memset(void *dst, int c, size_t n);
+int memcmp(const void *s1, const void *s2, size_t n);
+void *memcpy(void *dst, const void *src, size_t n);
+void *memmove(void *dst0, const void *src0, size_t length);
+int strcmp(const char *s1, const char *s2);
+size_t strlcat(char *dst, const char *src, size_t dsize);
+size_t strlcpy(char *dst, const char *src, size_t dsize);
+size_t strlen(const char *str);
+size_t strnlen(const char *str, size_t maxlen);
 
-int page_index_early_map(pgindex_t *index, addr_t paddr,
-	void *vaddr, size_t length)
-{
-	return -1;
-}
+#endif /* !__ASSEMBLER__ */
 
-void mmu_init(pgindex_t *boot_page_index)
-{
-}
+#endif
 
