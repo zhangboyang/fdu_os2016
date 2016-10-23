1657e2c14d851f93b2c9c54b5487340bb511bbf0
autocommit <autocommit@nowhere.com>
2016-10-23 16:46:06 +0800

    autocommit

--- kern/init/early_init.c
+++ kern/init/early_init.c
@@ -64,17 +64,16 @@ void master_early_init(void)
 	if (early_devices_init() < 0) {
 		panic("early_devices_init() failed.");
     }
-    
 	/* other preperations, including early secondary buses */
 	arch_early_init();
 	if (early_console_init(EARLY_CONSOLE_BUS, EARLY_CONSOLE_BASE, EARLY_CONSOLE_MAPPING) < 0) {
 		panic("Early console init failed.\n");
 	}
-
+bprintf("hahahaha1\n");
 	kputs("Hello, world!\n");
-	
+bprintf("hahahaha2\n");
 	early_mm_init();
-	
+bprintf("hahahaha3\n");
 	mmu_jump();
 }
 
