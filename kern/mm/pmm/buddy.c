8894aba7d7ce74bfc003d71902f558219b70ed30
autocommit <autocommit@nowhere.com>
2016-10-26 00:16:51 +0800

    autocommit

--- kern/mm/pmm/buddy.c
+++ kern/mm/pmm/buddy.c
@@ -180,6 +180,7 @@ void pmalloc_bootstrip(struct bootstrap_vmalloc *valloc)
     VF(pmm_zone[ZONE_DMA].allocator, free, 0x1000 * 3);
     kprintf("alloc = %016llx\n", VF(pmm_zone[ZONE_DMA].allocator, malloc, 0x1000 * 2));*/
 
+    VF(pmm_zone[ZONE_DMA].allocator, free, 0x1000 * 0);
     VF(pmm_zone[ZONE_DMA].allocator, free, 0x1000 * 1);
     VF(pmm_zone[ZONE_DMA].allocator, free, 0x1000 * 2);
     kprintf("alloc = %016llx\n", VF(pmm_zone[ZONE_DMA].allocator, malloc, 0x1000 * 2));
