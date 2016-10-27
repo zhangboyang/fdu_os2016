227c5ba044c46287efc7b61cbcba641661856159
autocommit <autocommit@nowhere.com>
2016-10-27 10:38:10 +0800

    autocommit

--- kern/mm/vmm/bootstrap.c
+++ kern/mm/vmm/bootstrap.c
@@ -77,7 +77,7 @@ void pmm_selftest()
         if (sz == 0) break;
         
         r = (1103515245 * r + 12345) & 0x7fffffff; // next rand
-        kprintf("got page paddr=%016llx vaddr=%08x\n", page, (unsigned) (page + KOFFSET)  );
+        kprintf("got page sz=%d paddr=%016llx vaddr=%08x\n", sz, page, (unsigned) (page + KOFFSET)  );
         
         for (i = 0; i < sz; i++) {
             int *x = (void *) (long)(page + i * 0x1000 + KOFFSET);
