b845b3d0012536d7b4bc3b928f41a605fb93a0e1
autocommit <autocommit@nowhere.com>
2016-10-30 23:09:39 +0800

    autocommit

--- kern/init/init.c
+++ kern/init/init.c
@@ -41,6 +41,15 @@ void master_init()
     // init IRQ
     trap_init();
     
+    int __attribute__((__noinline__)) syscall(int id, ...) {
+	    int ret;
+	    int *args = &id;
+	    asm volatile("int $0x80": "=a"(ret) : "a"(args[0]), "b"(args[1]), "c"(args[2]), "d"(args[3]), "esi"(args[4]), "edi"(args[5]), "ebp"(args[6]));
+	    return ret;
+    }
+    syscall(1,2,3,4,5,6,7);
+    
+    
     panic("bye");
 }
 
