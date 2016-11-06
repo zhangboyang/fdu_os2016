b10de2efb460eb5a3148954b000a0466b9cfea1f
autocommit <autocommit@nowhere.com>
2016-11-06 21:22:18 +0800

    autocommit

---
+++ lib/libc/strcmp.c
@@ -0,0 +1,12 @@
+/*
+ * this file is added by ZBY
+ */
+
+#include <libc/string.h>
+
+int strcmp(const char *s1, const char *s2)
+{
+    size_t l1 = strlen(s1), l2 = strlen(s2);
+    size_t len = MIN(l1, l2) + 1;
+    return memcmp(s1, s2, len);
+}
