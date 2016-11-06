0b5ddb32352a776f3886b4932a6085676b59af05
autocommit <autocommit@nowhere.com>
2016-11-06 21:23:01 +0800

    autocommit

--- lib/libc/strcmp.c
+++ lib/libc/strcmp.c
@@ -8,6 +8,6 @@
 int strcmp(const char *s1, const char *s2)
 {
     size_t l1 = strlen(s1), l2 = strlen(s2);
-    size_t len = min(l1, l2) + 1;
+    size_t len = MIN(l1, l2) + 1;
     return memcmp(s1, s2, len);
 }
