b7a396031f1505c0b5b4cde2b1a11d382b98c1c4
autocommit <autocommit@nowhere.com>
2016-11-06 20:47:37 +0800

    autocommit

--- lib/libc/strlcpy.c
+++ lib/libc/strlcpy.c
@@ -8,7 +8,7 @@ size_t strlcpy(char *dst, const char *src, size_t dsize)
 {
     size_t ret = strlen(src);
     if (dsize) {
-        while (dsize-- && !*src) *dst++ = *src++;
+        while (dsize-- && *src) *dst++ = *src++;
         *dst = '\0';
     }
     return ret;
