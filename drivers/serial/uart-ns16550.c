630d750cb4f9b7c145343c2bc5c1cf894d3d9c4d
autocommit <autocommit@nowhere.com>
2016-11-06 21:03:19 +0800

    autocommit

--- drivers/serial/uart-ns16550.c
+++ drivers/serial/uart-ns16550.c
@@ -191,6 +191,10 @@ int __early_console_init(struct bus_device *bus, addr_t base, addr_t mapped_base
 	return 0;
 }
 
+static int init()
+{
+    
+} INITCALL_DEV(init)
 #ifdef RAW /* baremetal driver */
 
 #else /* not RAW, or kernel driver */
