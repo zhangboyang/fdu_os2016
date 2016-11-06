7aed71d995a141f235540d8c5f1ce048e51007c0
autocommit <autocommit@nowhere.com>
2016-11-06 21:00:16 +0800

    autocommit

--- kern/init/initcalls.c
+++ kern/init/initcalls.c
@@ -18,9 +18,10 @@ int do_early_initcalls()
     kprintf("calling early initcalls:\n");
     initcall_t *st = LOWADDR(early_init_start), *ed = LOWADDR(early_init_end), *cur;
     for (cur = st; cur < ed; cur++) {
-        kprintf(" initcall %p: func=%p\n", cur, LOWADDR(*cur));
+        kprintf("  initcall %p: func=%p\n", cur, LOWADDR(*cur));
         if ((LOWADDR(*cur))() < 0) panic("early initcall %p failed.", LOWADDR(*cur));
     }
+    kprintf("early initcalls finished.\n");
     return 0;
 }
 int do_initcalls()
@@ -28,9 +29,10 @@ int do_initcalls()
     kprintf("calling normal initcalls:\n");
     initcall_t *st = norm_init_start, *ed = norm_init_end, *cur;
     for (cur = st; cur < ed; cur++) {
-        kprintf(" initcall %p: func=%p\n", cur, *cur);
-        if ((*cur)() < 0) panic("norm initcall %p failed.", *cur);
+        kprintf("  initcall %p: func=%p\n", cur, *cur);
+        if ((*cur)() < 0) panic("normal initcall %p failed.", *cur);
     }
+    kprintf("normal initcalls finished.\n");
     return 0;
 }
 
