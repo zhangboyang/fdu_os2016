44a33fe6457e67e85d0dc3d14c010d06f7e761ba
autocommit <autocommit@nowhere.com>
2016-11-06 17:43:34 +0800

    autocommit

---
+++ kern/init/initcalls.c
@@ -0,0 +1,26 @@
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif /* HAVE_CONFIG_H */
+
+#include <sys/types.h>
+#include <aim/panic.h>
+
+
+/*
+ * this file is added by ZBY
+ */
+
+int do_early_initcalls()
+{
+    initcall_t *st = &early_init_start, *ed = &early_init_end, *cur;
+    for (cur = st; cur < ed; cur++) {
+        if ((*cur)() < 0) panic("early initcall %p failed.", *cur);
+    }
+}
+int do_initcalls()
+{
+    initcall_t *st = &norm_init_start, *ed = &norm_init_end, *cur;
+    for (cur = st; cur < ed; cur++) {
+        if ((*cur)() < 0) panic("norm initcall %p failed.", *cur);
+    }
+}
