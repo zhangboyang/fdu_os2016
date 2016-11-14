9097282d095f686af03ca1db8c37ce6ff7599b5b
autocommit <autocommit@nowhere.com>
2016-11-14 19:17:35 +0800

    autocommit

---
+++ boot/arch/armv7a/console.c
@@ -0,0 +1,17 @@
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif /* HAVE_CONFIG_H */
+
+// this file is created by ZBY
+
+#include <sys/types.h>
+#include <arch-boot.h>
+
+
+void bioinit() // bootloader io system init
+{
+}
+
+void bputc(int c)
+{
+}
