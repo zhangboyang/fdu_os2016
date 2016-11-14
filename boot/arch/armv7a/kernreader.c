ac2030dd203afd907b3da17cfd20060e6d038f9d
autocommit <autocommit@nowhere.com>
2016-11-14 23:04:45 +0800

    autocommit

--- boot/arch/armv7a/kernreader.c
+++ boot/arch/armv7a/kernreader.c
@@ -11,13 +11,28 @@
  */
 
 
+struct bootloader_elf_info {
+    uint32_t elf_len;
+    uint32_t elf_crc32;
+};
+
+#define elf_info ((struct bootloader_elf_info *)(ELF_INFO_START))
+#define elf_data ((uint8_t *)(ELF_START))
+
 void kernreader_init(void)
 {
-
+    // check ELF crc32
+    uint32_t cur_crc32 = crc32(0, elf_data, elf_info->elf_len);
+    if (cur_crc32 != elf_info->elf_crc32) {
+        bpanic("crc32 mismatch, current %x, expected %x.", cur_crc32, elf_info->elf_crc32);
+    }
 }
 
 void kernreader_readfile(void *buf, size_t size, size_t offset)
 {
-
+    if (buf < (void *) (elf_data + elf_info->elf_len)) {
+        bpanic("phdr overlap detected, buf=%p elfend=%p size=%x offset=%x.", buf, elf_data + elf_info->elf_len, size, offset);
+    }
+    bmemcpy(buf, elf_data + offset, size);
 }
 
