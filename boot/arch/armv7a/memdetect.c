7b1b74da3954953d04eda6cf646804cd7f83d7d2
autocommit <autocommit@nowhere.com>
2016-11-14 19:18:35 +0800

    autocommit

---
+++ boot/arch/armv7a/memdetect.c
@@ -0,0 +1,17 @@
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif /* HAVE_CONFIG_H */
+
+#include <sys/types.h>
+#include <aim/boot.h>
+#include <arch-boot.h>
+
+/*
+ * this file is added by ZBY
+ */
+
+void detectmemory()
+{
+    // dummy, do nothing here
+}
+
