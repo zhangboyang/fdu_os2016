849121df936cfcb8359f01099fba5d0c34c2805e
autocommit <autocommit@nowhere.com>
2016-11-15 13:32:10 +0800

    autocommit

---
+++ drivers/serial/uart-rpi2.c
@@ -0,0 +1,41 @@
+/* Copyright (C) 2016 Gan Quan <coin2028@hotmail.com>
+ * Copyright (C) 2016 David Gao <davidgao1001@gmail.com>
+ *
+ * This file is part of AIM.
+ *
+ * AIM is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * AIM is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <sys/types.h>
+#include <aim/console.h>
+#include <aim/device.h>
+#include <aim/mmu.h>
+
+int __early_console_init(struct bus_device *bus, addr_t base, addr_t mapped_base)
+{
+	// FIXME
+
+	return 0;
+}
+
+#ifdef RAW /* baremetal driver */
+
+#else /* not RAW, or kernel driver */
+
+#endif /* RAW */
+
