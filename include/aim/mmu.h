0996614878513aaf5b12344204e62b8cb617dc0a
autocommit <autocommit@nowhere.com>
2016-11-15 13:18:00 +0800

    autocommit

--- include/aim/mmu.h
+++ include/aim/mmu.h
@@ -26,6 +26,13 @@
 #define KOFFSET KERN_BASE
 #define KTOP 0xFE000000
 #define LOWADDR(highaddr) ((typeof((highaddr) + 0)) ((ULCAST(highaddr)) - KERN_BASE))
+/* addresses before and after early MMU mapping */
+#define __premap_addr(kva)	(ULCAST(kva) - KERN_BASE)
+#define __postmap_addr(pa)	(ULCAST(pa) + KERN_BASE)
+/* kernel virtual address and physical address conversion */
+#define kva2pa(kva)		(ULCAST(kva) - KERN_BASE)
+#define pa2kva(pa)		(PTRCAST(pa) + KERN_BASE)
+
 
 #ifndef PAGE_SIZE
 #define PAGE_SIZE	4096
