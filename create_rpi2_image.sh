da53a98116aa2b5402f36d4bf56ab6a4d62f0e14
autocommit <autocommit@nowhere.com>
2016-11-15 18:15:20 +0800

    autocommit

--- create_rpi2_image.sh
+++ create_rpi2_image.sh
@@ -19,7 +19,7 @@ echo -ne '\x1b[0m'
 cp boot/boot.bin kernel7.img
 
 BOOTLOADER_START=0x8000
-ELF_INFO_START=0x18000
+ELF_INFO_START=0x10000
 ELF_START=0x20000
 
 (int2bin `stat --printf="%s" kern/vmaim.elf` && int2bin 0x`crc32 kern/vmaim.elf`) | dd of=kernel7.img bs=512 seek=$[($ELF_INFO_START-$BOOTLOADER_START)/512]
