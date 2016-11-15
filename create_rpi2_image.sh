6fbebdd3804cfcecff4c9779c303ecfcd34a9002
autocommit <autocommit@nowhere.com>
2016-11-15 14:42:53 +0800

    autocommit

--- create_rpi2_image.sh
+++ create_rpi2_image.sh
@@ -8,6 +8,7 @@ function fail()
 function int2bin()
 {
     echo -e "#include<stdio.h>\nmain(){int x=$@;fwrite(&x,4,1,stdout);}" | tcc -run -
+    true
 }
 
 echo -ne '\x1b[1m\x1b[31m'
@@ -18,6 +19,7 @@ echo -ne '\x1b[0m'
 cp boot/boot.bin kernel7.img
 
 ELF_INFO_START=0x10000
+(int2bin `stat --printf="%s" kern/vmaim.elf` && int2bin 0x`crc32 kern/vmaim.elf`) | dd of=kernel7.img bs=512 seek=$[$ELF_INFO_START/512]
 
-#(int2bin `stat --printf="%s" kern/kern.elf` && int2bin 0x`crc32 kern/vmaim.elf`)
-#dd of=kernel7.img bs=512 skip=$[$ELF_INFO_START/512]
+ELF_START=0x20000
+dd if=kern/vmaim.elf of=kernel7.img bs=512 seek=$[$ELF_START/512]
--- include/arch/armv7a/asm.h
+++ include/arch/armv7a/asm.h
@@ -1,5 +1,11 @@
 #ifndef _ASM_H
 #define _ASM_H
 
+static inline uint32_t inl(uint32_t addr)
+{
+    uint32_t ret;
+    __asm__ __volatile__ ("ldr %0,[%1]":"=r"(ret):"r"(addr));
+	return 0;
+}
 
 #endif
