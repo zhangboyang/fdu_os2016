e3b7fdd05b03e4f99798888f217de6b2514cc9fe
autocommit <autocommit@nowhere.com>
2016-11-17 21:37:04 +0800

    autocommit

--- drivers/serial/uart-rpi2.c
+++ drivers/serial/uart-rpi2.c
@@ -25,6 +25,8 @@ static void __uart_rpi2_init(struct chr_device *inst)
     struct bus_device *bus = inst->bus;
 	bus_write_fp bus_write32 = bus->bus_driver.get_write_fp(bus, 32);
 	if (!bus_write32) while (1);
+	bus_write_fp bus_read32 = bus->bus_driver.get_read_fp(bus, 32);
+	if (!bus_read32) while (1);
 	
 	// do init staff
 	unsigned int ra;
@@ -37,7 +39,7 @@ static void __uart_rpi2_init(struct chr_device *inst)
     bus_write32(bus, inst->base, AUX_MU_IER_REG, 0);
     bus_write32(bus, inst->base, AUX_MU_IIR_REG, 0xC6);
     bus_write32(bus, inst->base, AUX_MU_BAUD_REG, 270);
-    ra = inl(GPFSEL1);
+    ra = bus_read32(bus, inst->base, GPFSEL1);
     ra &= ~(7 << 12); //gpio14
     ra |= 2 << 12;    //alt5
     ra &= ~(7 << 15); //gpio15
