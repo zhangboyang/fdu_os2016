595ab47b35a73b4cde7737d6f3057a3789f14a55
autocommit <autocommit@nowhere.com>
2016-11-18 19:36:59 +0800

    autocommit

--- kern/arch/armv7a/mach-rpi2/mailbox.c
+++ kern/arch/armv7a/mach-rpi2/mailbox.c
@@ -92,7 +92,7 @@ int ask_property(void *buf, size_t reqsize, size_t bufsize)
     memcpy(__property_buffer_header.data, buf, min(reqsize, PROPERTY_DATASIZE));
     memset(__property_buffer_header.data + min(reqsize, PROPERTY_DATASIZE), 0, 4); // set end tag
     int r;
-    if ((r = write_mailbox(MAILBOX_CHANNEL_PROPERTY, ULCAST(&__property_buffer_header))) < 0) return r;
+    if ((r = write_mailbox(MAILBOX_CHANNEL_PROPERTY, PA2VCA(premap_addr(ULCAST(&__property_buffer_header))))) < 0) return r;
     if ((r = read_mailbox(MAILBOX_CHANNEL_PROPERTY, NULL)) < 0) return r;
     memcpy(buf, __property_buffer_header.data, min(bufsize, PROPERTY_DATASIZE));
     return 0;
