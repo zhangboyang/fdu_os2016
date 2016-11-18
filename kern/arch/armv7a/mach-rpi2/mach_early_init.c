0e0361e1580ed97041be7a8b2638464ac06f98b1
autocommit <autocommit@nowhere.com>
2016-11-18 20:29:48 +0800

    autocommit

--- kern/arch/armv7a/mach-rpi2/mach_early_init.c
+++ kern/arch/armv7a/mach-rpi2/mach_early_init.c
@@ -14,10 +14,10 @@ void mach_early_init()
         uint32_t base;
         uint32_t size;
     } arminfo, vcinfo;
-    if (ask_property_tag(MAILBOX_PROP_ARMMEMORY, &arminfo, 0, sizeof(arminfo)) < 0) panic("can't get memory info for ARM");
-    if (ask_property_tag(MAILBOX_PROP_VCMEMORY, &vcinfo, 0, sizeof(vcinfo)) < 0) panic("can't get memory info for VideoCore");
+    if (ask_property_tag(MAILBOX_PROP_ARMMEMORY, &arminfo, 0, sizeof(arminfo), NULL) < 0) panic("can't get memory info for ARM");
+    if (ask_property_tag(MAILBOX_PROP_VCMEMORY, &vcinfo, 0, sizeof(vcinfo), NULL) < 0) panic("can't get memory info for VideoCore");
     
-    kprintf("arm memory: base=0x%x size=0x%x\n", r, arminfo.base, arminfo.size, NULL);
-    kprintf("vc memory: base=0x%x size=0x%x\n", r, vcinfo.base, vcinfo.size, NULL);
+    kprintf("arm memory: base=0x%x size=0x%x\n", r, arminfo.base, arminfo.size);
+    kprintf("vc memory: base=0x%x size=0x%x\n", r, vcinfo.base, vcinfo.size);
     
 }
