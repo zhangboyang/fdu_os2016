a8050d94c03683796ac283eb02f28bc77e29a561
autocommit <autocommit@nowhere.com>
2016-11-23 20:25:05 +0800

    autocommit

--- kern/arch/armv7a/arch_mmu.c
+++ kern/arch/armv7a/arch_mmu.c
@@ -72,7 +72,7 @@ int page_index_early_map(pgindex_t *boot_page_index, addr_t paddr, void *vaddr, 
         // map VA to PA
         pgmid_t *pgmid = WKPGINDEX(boot_page_index[PGINDEX_FN(va)]);
         pgmid[PGMID_FN(va)] = MKPGMID_BIG(pa);
-        kprintf("pgmid=%llx midfn=%llx val=%llx\n", pgmid, PGMID_FN(va), pgmid[PGMID_FN(va)]);
+        kprintf("pgmid=%llx midfn=%llx val=%llx\n", ULLCAST(pgmid), ULLCAST(PGMID_FN(va)), ULLCAST(pgmid[PGMID_FN(va)]));
     }
     
     // FIXME: device memory attr
