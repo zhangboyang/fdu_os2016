d2f3217df22f273274f750b3ef228a13c1df925d
autocommit <autocommit@nowhere.com>
2016-11-24 21:19:54 +0800

    autocommit

--- kern/arch/armv7a/arch_mmu.c
+++ kern/arch/armv7a/arch_mmu.c
@@ -195,7 +195,7 @@ fbcls(LOWADDR(&fb), 0xff);
     kprintf("new SCTLR = %08x\n", SCTLR.val);
     __asm__ __volatile__ ("mcr p15, 0, %0, c1, c0, 0"::"r"(SCTLR.val));
     
-fbcls(LOWADDR(&fb), 0xffff);    
+fbcls(LOWADDR(&fb), 0xff00ff);    
 
     
     kprintf("MMU enabled ...\n");
