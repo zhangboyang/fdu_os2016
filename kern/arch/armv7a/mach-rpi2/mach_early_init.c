f7b42b1eb880a676b6cc9bb5d6a490b48e2bf30e
autocommit <autocommit@nowhere.com>
2016-11-24 18:19:50 +0800

    autocommit

--- kern/arch/armv7a/mach-rpi2/mach_early_init.c
+++ kern/arch/armv7a/mach-rpi2/mach_early_init.c
@@ -9,7 +9,7 @@
 #include <platform.h>
 
 
-__asm__ ("splash_image_data:\n.incbin \"splash.rgb\"\n");
+//__asm__ ("splash_image_data:\n.incbin \"splash.rgb\"\n");
 
 //__asm__ ("jtxj:\n.incbin \"jtxj.rgb\"\n");
 
@@ -71,7 +71,7 @@ void mach_early_init()
     memset(fbdev.bits, -1, fbdev.height * fbdev.pitch);
     dump_memory(fbdev.bits, 0xA0);*/
     
-    extern uint8_t splash_image_data[]; show_splash(&fbdev, LOWADDR(splash_image_data), 175, 100, 24);
+//    extern uint8_t splash_image_data[]; show_splash(&fbdev, LOWADDR(splash_image_data), 175, 100, 24);
     
 //    extern uint8_t jtxj[]; show_splash(&fbdev, LOWADDR(jtxj), 318, 346, 24);
 }
