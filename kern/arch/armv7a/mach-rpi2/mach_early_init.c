1c2597004d44df7223178b4739431d7be521a440
autocommit <autocommit@nowhere.com>
2016-11-24 23:43:22 +0800

    autocommit

--- kern/arch/armv7a/mach-rpi2/mach_early_init.c
+++ kern/arch/armv7a/mach-rpi2/mach_early_init.c
@@ -63,14 +63,14 @@ static void rpi2_detect_memory()
 static void rpi2_fbdev_jumphandler(void)
 {
     fb.bits += KOFFSET;
+//    extern uint8_t splash_image_data[]; show_splash(LOWADDR(&fb), LOWADDR(splash_image_data), 175, 100, 24);
+//    extern uint8_t jtxj[]; show_splash(&fbdev, LOWADDR(jtxj), 318, 346, 24);
 }
 static void rpi2_fbdev_init()
 {
     if (fbinit(LOWADDR(&fb)) < 0) panic("can't init framebuffer");
     jump_handlers_add(rpi2_fbdev_jumphandler);
     fbcls(LOWADDR(&fb), 0x00ff00);
-//    extern uint8_t splash_image_data[]; show_splash(LOWADDR(&fb), LOWADDR(splash_image_data), 175, 100, 24);
-//    extern uint8_t jtxj[]; show_splash(&fbdev, LOWADDR(jtxj), 318, 346, 24);
 }
 
 void mach_early_init()
