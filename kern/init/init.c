aad1f1b75e326ed825aab84d40f751ad788b1b82
autocommit <autocommit@nowhere.com>
2016-11-25 00:33:29 +0800

    autocommit

--- kern/init/init.c
+++ kern/init/init.c
@@ -56,6 +56,7 @@ __asm__ ( // the syscall()
 void master_init()
 {
 fbcls(&fb, 0xffff00);
+kprintf("fb=%p\n", fb.bits);
 //fbdrawch(&fb, 0, 0, 0xFFFFFF, 0, 'A');
     bprintf("this is bprintf, at highaddr!\n");
     
