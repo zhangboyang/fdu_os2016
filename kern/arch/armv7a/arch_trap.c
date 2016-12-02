42633cf592fca7ee838e46237c7362c719ec0f54
autocommit <autocommit@nowhere.com>
2016-12-02 18:54:25 +0800

    autocommit

--- kern/arch/armv7a/arch_trap.c
+++ kern/arch/armv7a/arch_trap.c
@@ -19,12 +19,7 @@
 
 void trap_init()
 {
-    dump_memory((void *) 0, 0x100);
-    dump_memory_aligned((void *) 0, 0x100);
-    dump_memory((void *) 5, 0x100);
-    dump_memory_aligned((void *) 5, 0x100);
-    dump_memory((void *) 15, 0x100);
-    dump_memory_aligned((void *) 15, 0x100);
+    __asm__ __volatile__ ("mcr p15, 0, %0, c12, c0, 0"::"r"(vectors));
 }
 
 void trap(struct trapframe *tf)
