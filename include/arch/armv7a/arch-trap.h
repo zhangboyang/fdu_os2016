6000ce6d5f8687ba19ada09de110ebe5b19b7a36
autocommit <autocommit@nowhere.com>
2016-12-02 20:55:32 +0800

    autocommit

--- include/arch/armv7a/arch-trap.h
+++ include/arch/armv7a/arch-trap.h
@@ -19,7 +19,21 @@
 #ifndef _ARCH_TRAP_H
 #define _ARCH_TRAP_H
 
+#define MODE_USR        0x10
+#define MODE_FIQ        0x11
+#define MODE_IRQ        0x12
+#define MODE_SVC        0x13
+#define MODE_MON        0x16
+#define MODE_ABT        0x17
+#define MODE_HYP        0x1A
+#define MODE_UND        0x1B
+#define MODE_SYS        0x1F
+
+#define MODE_MASK       0x1F
+
+
 #ifndef __ASSEMBLER__
+
 
 struct trapframe {
 };
