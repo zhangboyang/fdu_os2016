fd5110a4c5dab1e2e6d19dd6cee00177cb16847d
autocommit <autocommit@nowhere.com>
2016-12-02 22:27:43 +0800

    autocommit

--- kern/arch/armv7a/arch_trap.c
+++ kern/arch/armv7a/arch_trap.c
@@ -22,7 +22,23 @@ void trap_init()
 {
     __asm__ __volatile__ ("mcr p15, 0, %0, c12, c0, 0"::"r"(vectors));
     kprintf("haha\n");
-    __asm__ __volatile__ ("svc #0");
+    __asm__ __volatile__ ("mov r0, #0;"
+                          "mov r1, #1;"
+                          "mov r2, #2;"
+                          "mov r3, #3;"
+                          "mov r4, #4;"
+                          "mov r5, #5;"
+                          "mov r6, #6;"
+                          "mov r7, #7;"
+                          "mov r8, #8;"
+                          "mov r9, #9;"
+                          "mov r10, #10;"
+                          "mov r11, #11;"
+                          "mov r12, #12;"
+                          "mov r13, #13;"
+                          "mov r14, #14;"
+//                          "mov r15, #15;"
+                          "svc #0");  
     kprintf("bad\n");
 }
 
