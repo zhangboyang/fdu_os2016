d56e30c0a59e483f23de2b922544d6b409a9ceea
autocommit <autocommit@nowhere.com>
2016-12-04 17:37:20 +0800

    autocommit

--- kern/init/early_init.c
+++ kern/init/early_init.c
@@ -125,6 +125,5 @@ void slave_early_init(int id)
 //framebuffer: base=3d800000 width=1440 height=900 pitch=000010e0 format=0
     memset((void *)0x3d800000, 0xff, 900*0x000010e0);
     arch_slave_early_init(id);
-    memset((void *)0x3d800000, 0, 900*0x000010e0);
     mmu_jump(slave_entry);
 }
