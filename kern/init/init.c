2b4f178ae004914fb90ece3b048145d76f8d4a56
autocommit <autocommit@nowhere.com>
2016-12-04 19:03:29 +0800

    autocommit

--- kern/init/init.c
+++ kern/init/init.c
@@ -117,7 +117,7 @@ while(1);;;
 
 void slave_init(int id)
 {
-    if (0 <= id && id < 4)
+    if (id == 1)
     memset((void *)0x3d800000 + KOFFSET, 0xff, 900*0x000010e0);
     else
     memset((void *)0x3d800000 + KOFFSET, 0x00, 900*0x000010e0);
