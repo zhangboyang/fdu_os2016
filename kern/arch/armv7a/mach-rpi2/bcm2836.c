85a490360ef96c93eae1341f358450de4ad8dc7a
autocommit <autocommit@nowhere.com>
2016-12-04 16:18:34 +0800

    autocommit

--- kern/arch/armv7a/mach-rpi2/bcm2836.c
+++ kern/arch/armv7a/mach-rpi2/bcm2836.c
@@ -36,7 +36,7 @@ void bcm2836_writereg(uint32_t reg, uint32_t data)
 	bus_write_fp bus_write32 = bus->bus_driver.get_write_fp(bus, 32);
 	if (!bus_write32) return -1;
 	
-    bus_write32(bus, inst->base, REG, data);
+    bus_write32(bus, inst->base, reg, data);
 }
 
 void bcm2836_init()
