96d1955aee209cd4bd4d0904be46f2e0d2700331
autocommit <autocommit@nowhere.com>
2016-12-04 18:45:50 +0800

    autocommit

--- kern/init/init.c
+++ kern/init/init.c
@@ -118,7 +118,7 @@ while(1);;;
 void slave_init(int id)
 {
     memset((void *)0x3d800000 + KOFFSET, 0xff, 900*0x000010e0);
-//    fbcls(&fb, 0xff00);
+    fbcls(&fb, 0xff00);
     memset((void *)0x3d800000 + KOFFSET, 0x00, 900*0x000010e0);
     
     while (1);
